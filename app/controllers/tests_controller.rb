69b831258c55083ac518fcde1e703a59dc67e0b3
lena <rublen77@gmail.com>
2018-09-10 23:39:31 +0300

    without logger

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,20 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    # render plain: "Hello from Simpler!!"
+    render 'text/html' => '<h1> Hi there!</h1>'
+    # render 'image/png' => 'picture.png'
+    # render 'tests/list'
+    status 201
+    headers['Content-Type'] = 'text/plain'
   end
 
   def create
 
+  end
+
+  def show
+    @test = Test.find(id: params[:id].to_i)
   end
 
 end
