8e8692df5ff9ceabd087d95bd2038fb48c84ab41
lena <rublen77@gmail.com>
2018-09-12 10:54:58 +0300

    task: corrections

--- middleware/logger.rb
+++ middleware/logger.rb
@@ -15,10 +15,13 @@ class AppLogger
 
   def log_data(env)
     request = Rack::Request.new(env)
+    template = "#{request.env['simpler.template']}.html.erb" if request.env['simpler.template']
+
     data ="Request: #{request.request_method} #{request.path}\n" +
       "Handler: #{request.env['simpler.controller'].name.capitalize}Controller##{request.env['simpler.action']}\n" +
       "Parameters: #{request.params.merge(request.env['simpler.route_params'])}\n" +
-      "Response: #{@response[0]} #{@response[1]['Content-type']} #{request.env['simpler.template']}.html.erb\n"
+      "Response: #{@response[0]} #{@response[1]['Content-type']} #{template}\n"
+
     @logger.info(data)
   end
 
